e2551756815f146ffc742c656704b611550e000c
hieu <mendyo125e@gmail.com>
2024-12-22 15:12:08 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -25,8 +25,10 @@ def lambda_handler(event=None, context=None):
     #time.sleep(1)
     ossytem=os.system("ls /")
     print(ossytem)
-    ossytem=os.system("ls /tmp")
-    print(ossytem)
+    ossytem1=os.system("ls /tmp")
+    print(ossytem1)
+    ossytem2=os.system("ls /tmp/")
+    print(f"=================={ossytem2}===============")
     title="ok 123"
 
     return {
