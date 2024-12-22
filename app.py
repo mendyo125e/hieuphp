5017d1c68ebbd23d68a4516c75c656189fbd501c
hieu <mendyo125e@gmail.com>
2024-12-22 14:48:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
+import urllib3,urllib.parse
 import random
 import time,json
 import unittest
